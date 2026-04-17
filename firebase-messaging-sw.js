1f4dd05b73f1e8966371d14def1aa009103c2dee
thnboard-coder <qkr1655516@gmail.com>
2026-04-17 00:02:03 -0700

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC2II3ydXUE2CRJWtYTx5yf0W4kxT8qgrs",
+  authDomain: "thn-guild.firebaseapp.com",
+  projectId: "thn-guild",
+  storageBucket: "thn-guild.firebasestorage.app",
+  messagingSenderId: "130528607870",
+  appId: "1:130528607870:web:c0d477110c5a183270e22f"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  const title = payload.notification.title || 'THN Guild';
+  const body  = payload.notification.body  || '';
+  self.registration.showNotification(title, {
+    body: body,
+    icon: '/icon.png',
+    badge: '/icon.png',
+    vibrate: [300,100,300,100,300],
+    requireInteraction: false
+  });
+});
