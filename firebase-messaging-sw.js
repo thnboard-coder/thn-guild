895f884894bfb01ba60872f8042363007496ec75
thnboard-coder <qkr1655516@gmail.com>
2026-04-17 21:14:02 -0700

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,21 +9,6 @@ firebase.initializeApp({
   appId: "1:130528607870:web:c0d477110c5a183270e22f"
 });
 const messaging = firebase.messaging();
-messaging.onBackgroundMessage(function(payload) {
-  const title = (payload.notification && payload.notification.title) || 'THN Guild';
-  const body  = (payload.notification && payload.notification.body) || '';
-  return self.clients.matchAll({type:'window', includeUncontrolled:true}).then(function(clients){
-    if(clients && clients.length > 0) return;
-    return self.registration.showNotification(title, {
-      body: body,
-      icon: '/thn-guild/icon.png',
-      vibrate: [300,100,300,100,300],
-      requireInteraction: true,
-      tag: 'thn-alert',
-      renotify: true
-    });
-  });
-});
 self.addEventListener('notificationclick', function(e){
   e.notification.close();
   e.waitUntil(clients.openWindow('/thn-guild/'));
